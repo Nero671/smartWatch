c25a6216307032ebff0afe5eed223c89f0de8b2e
Nero671 <32450586+Nero671@users.noreply.github.com>
2020-07-06 23:54:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var dots = document.getElementsByClassName('dots-item'),
+  dotsArea = document.getElementsByClassName('dots-block')[0],
+  slides = document.getElementsByClassName('slides-item'),
+  prevBtn = document.getElementById('left-button'),
+  nextBtn = document.getElementById('right-button'),
+  slideIndex = 1
+
+showSlides(slideIndex)
+
+function showSlides(n) {
+  if (n < 1) {
+    slideIndex = slides.length
+  } else if (n > slides.length) {
+    slideIndex = 1
+  }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active')
+  }
+  slides[slideIndex - 1].style.display = 'flex'
+  dots[slideIndex - 1].classList.add('active')
+}
+
+function plussSlide(n) {
+  showSlides(slideIndex += n)
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+prevBtn.onclick = function () {
+  plussSlide(-1)
+}
+
+nextBtn.onclick = function () {
+  plussSlide(1)
+}
+
+dotsArea.onclick = function (e) {
+  for (var i = 0; i < dots.length + 1; i++) {
+    if (e.target.classList.contains('dots-item') && e.target == dots[i - 1]) {
+      currentSlide(i)
+    }
+  }
+} 
